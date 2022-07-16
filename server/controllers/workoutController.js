c9017848419ecd979250d4b84f424552e5e68753
Adnan Zaib <adnanzaib6@gmail.com>
2022-07-16 19:20:39 +0500

    Version 1.0 completed

---
+++ server/controllers/workoutController.js
@@ -0,0 +1,127 @@
+const Workout = require("../models/workoutModel");
+const mongoose = require("mongoose");
+
+// get all
+const getWorkouts = async (req, res) => {
+  try {
+    const workouts = await Workout.find({}).sort({ createdAt: -1 }); // for descending order
+
+    return res.status(200).json(workouts);
+  } catch (error) {
+    console.log("Error getting all workouts ", error);
+    return res
+      .status(500)
+      .json({ message: "Error getting all workouts! ", error: error.message });
+  }
+};
+
+// get by id
+const getWorkout = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose?.Types?.ObjectId?.isValid(id)) {
+    return res.status(400).json({ message: "Invalid id!" });
+  }
+  try {
+    const workout = await Workout.findById(id);
+    if (!workout) {
+      return res.status(404).json({ message: "Workout not found." });
+    }
+    return res.status(200).send(workout);
+  } catch (error) {
+    console.log("Error getting single workout", error);
+    return res
+      .status(500)
+      .json({ message: "Error getting workout! ", error: error.message });
+  }
+};
+
+// create new
+const createWorkout = async (req, res) => {
+  const { title, reps, load } = req.body;
+  let emptyFields = [];
+  if (!title) {
+    emptyFields.push("title");
+  }
+  if (!load) {
+    emptyFields.push("load");
+  }
+  if (!reps) {
+    emptyFields.push("reps");
+  }
+  if (emptyFields?.length > 0) {
+    return res
+      .status(400)
+      .json({ error: "Please fill all the fields", emptyFields });
+  }
+  try {
+    const workout = await Workout.create({
+      title,
+      reps,
+      load,
+    });
+    return res.status(200).json(workout);
+  } catch (error) {
+    console.log("Error creating workout", error);
+    return res
+      .status(500)
+      .json({ message: "Error creating workout! ", error: error.message });
+  }
+};
+
+// delete by id
+const deleteWorkout = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose?.Types?.ObjectId?.isValid(id)) {
+    return res.status(400).json({ message: "Invalid id!" });
+  }
+  try {
+    const workout = await Workout.findOneAndDelete({ _id: id });
+
+    if (!workout) {
+      return res.status(404).json({ message: "Workout not found." });
+    }
+    return res
+      .status(200)
+      .json({ message: "Workout deleted.", deletedWorkout: workout });
+  } catch (error) {
+    console.log("Error deleting workout", error);
+    return res
+      .status(500)
+      .json({ message: "Error deleting workout! ", error: error.message });
+  }
+};
+
+// update by id
+const updateWorkout = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose?.Types?.ObjectId?.isValid(id)) {
+    return res.status(400).json({ message: "Invalid id!" });
+  }
+  try {
+    const workout = await Workout.findOneAndUpdate(
+      { _id: id },
+      {
+        ...req.body,
+      },
+      { new: true }
+    );
+    if (!workout) {
+      return res.status(404).json({ message: "Workout not found." });
+    }
+    return res
+      .status(200)
+      .json({ message: "Workout updated.", updatedWorkout: workout });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error deleting workout! ", error: error.message });
+  }
+};
+
+module.exports = {
+  getWorkouts,
+  getWorkout,
+  createWorkout,
+  deleteWorkout,
+  updateWorkout,
+};
---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+require("dotenv").config();
+const workoutRoutes = require("./routes/workouts");
+const mongoose = require("mongoose");
+
+// express app
+const app = express();
+
+// to use middleware
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// router handler to react to requests
+app.use("/api/workouts", workoutRoutes);
+
+// connect to db
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => {
+    // listen for api requests on port
+    app.listen(process.env.PORT, () => {
+      console.log("Connected to DB successfully!");
+      console.log("Listening on port 3001!");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
